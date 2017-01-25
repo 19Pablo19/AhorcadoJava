b30d95188bf32457537ce30c3ecc5535152e8ef7
Pablo <Pablo@169.254.195.7>
2017-01-25 13:00:42 +0100

    En esta version el programa cambia las letras de la pantalla con un StringBuilder

---
+++ src/codigo/ImageIcon.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.net.URL;
+
+/**
+ *
+ * @author Pablo
+ */
+class ImageIcon {
+
+    ImageIcon(URL url) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
